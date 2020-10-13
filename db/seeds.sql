d2c3e45635ff75c97959d491969fe0478af86055
JLC <jlc@annonymous.com>
2020-10-13 11:18:41 -0500

    Adding starting rows to the burguers table

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (`name`,`devoured`) VALUES("Quesadilla Burger", 0);
+INSERT INTO burgers (`name`,`devoured`) VALUES("Macho Alfa Burger", 0);
+INSERT INTO burgers (`name`,`devoured`) VALUES("MIDWEST Burger", 0);
+INSERT INTO burgers (`name`,`devoured`) VALUES("COASTAL Burger", 0);
+INSERT INTO burgers (`name`,`devoured`) VALUES("HONDUREAN Burger", 0);
