442a6a6f5ed9062fa41e21898f18c302fedb9dc8
JLC <jlc@annonymous.com>
2020-10-13 11:40:45 -0500

    Adding the handmade ORM to this app yumm..

---
+++ config/orm.js
@@ -0,0 +1,18 @@
+const connection = require("./connection.js");
+
+const orm = {
+    selectAll: function({}) {
+
+    },
+
+    insertOne: function({}) {
+
+    },
+
+    updateOne: function({}) {
+
+    }
+}
+
+
+module.exports = orm;
