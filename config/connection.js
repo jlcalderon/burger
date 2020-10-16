a8cb7146098e31fbfa9941e502c64f061746ed02
JLC <jlc@annonymous.com>
2020-10-16 11:27:28 -0500

    Configuring connection.js to connect data base with heroku jawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,16 @@
 const mysql = require("mysql");
+const connection;
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "",
+        database: "burgers_db"
+    });
+};
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "",
-    database: "burgers_db"
-});
-
-connection.connect(function(err) {
-    if (err) {
-        console.log(`Error connecting to: ${err.stack}`);
-        return;
-    }
-    console.log(`Connected as id ${connection.threadId} `);
-});
-
-console.log(`to database: ${connection.database} `);
-
+connection.connect();
 module.exports = connection;
