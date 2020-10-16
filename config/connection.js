57d022ce584c53e499068f367939c71429ec75d9
JLC <jlc@annonymous.com>
2020-10-16 15:02:41 -0500

    Fixing initialization of connection in line 2 of connections.js

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,5 @@
 const mysql = require("mysql");
-const connection;
+const connection = {};
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
@@ -11,6 +11,12 @@ if (process.env.JAWSDB_URL) {
         database: "burgers_db"
     });
 };
-
-connection.connect();
+// Make connection.
+connection.connect(function(err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
 module.exports = connection;
